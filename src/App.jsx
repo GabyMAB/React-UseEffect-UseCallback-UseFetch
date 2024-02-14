b0f3a93c6e1e51671395e5b7f17c86693fd78bec
gaby <gaby2001monse@outlook.com>
2024-02-13 18:21:55 -0600

    comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,29 @@ import { useState } from "react";
 import { useFetch } from "./hook/useFetch";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0); //declara el estado del contador en cero y la funcion para actualizar ese estado
 
-  const { data, loading, error } = useFetch(
+  const { data, loading, error } = useFetch( // hace la llamada a la API que contendra tres valores
     "https://jsonplaceholder.typicode.com/users"
   );
 
-  if (loading) {
-    return <h1>Loading...</h1>;
+  if (loading) { //si esta cargando 
+    return <h1>Loading...</h1>; //mostrara ese mensaje
   }
 
   if (error) {
-    return <h1>{error}</h1>;
+    return <h1>{error}</h1>; //mostrara el mensaje de error
   }
 
   return (
     <>
       <h1>useEffect</h1>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={() => setCounter(counter + 1)}> {/*si no hay errores incrementara el contador */}
         incremento {counter}
       </button>
       <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
+        {data.map((user) => ( 
+          <li key={user.id}>{user.name}</li> //regresara la lista de elementos del user
         ))}
       </ul>
     </>
--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,24 +1,24 @@
 import { useCallback, useEffect, useState } from "react";
 
-export const useFetch = (url) => {
-  console.log("useFetch");
+export const useFetch = (url) => { //se crea la funcion tomando a url
+  console.log("useFetch"); // imprime la palabra useFetch
 
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [data, setData] = useState(null); //declara es estado de data y setdata 
+  const [loading, setLoading] = useState(true); //declara el estado
+  const [error, setError] = useState(null); //declara el estado
 
-  const getData = useCallback(async () => {
+  const getData = useCallback(async () => { //crear una funcion y hara la llamada 
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Error fetching data");
+      const response = await fetch(url); //crea la promesa esperando la respuesta de la url
+      if (!response.ok) { //si se cumple se guardan los datos 
+        throw new Error("Error fetching data"); //si no se cumple nuestra un mensaje de error
       }
-      const data = await response.json();
+      const data = await response.json(); //si se cumple se guardan los datos en formato json 
       setData(data);
     } catch (error) {
       setError(error.message);
     } finally {
-      setLoading(false);
+      setLoading(false); //mensaje de que el estado ha finalizado
     }
   }, [url]);
 
